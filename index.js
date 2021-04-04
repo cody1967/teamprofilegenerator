e475af226efa833533d946c46f89637b6e235dda
Cody McNeill <cody@utahhydrographics.com>
2021-04-04 13:21:46 -0600

    updated index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 //  Need to get input from the user- inquirer
-const inquirer = require("inquirer");
-const { fstat } = require("node:fs"); 
-const Employee = require('../lib/Employee');
+const inquirer = require("inquirer"); 
+const Employee = require("./lib/Employee");
+const tempy = require("./src/templete")
 // require intern,manager,Engineer
 // require path
 inquirer.prompt([
@@ -9,8 +9,15 @@ inquirer.prompt([
     type:"input",
     name:"name",
     message:"What is the employee name?"
-  
+      
+  },
+  {
+    type:"input",
+    name:"email",
+    message:"What is your email?"
+      
   }
+  
 // wrap all the prompts in a function console questions call at end 
 
 
@@ -18,7 +25,11 @@ inquirer.prompt([
   console.log("resp", response)
 
 // creat employee objects from input
-const empy = new Employee(response.name, 0, email)
+const empy = new Employee(response.name, 0, response.email)
+const html = tempy()
+
+console.log(html)
+
 })
 // create html from the emplyee objects 
 
--- src/templete.js
+++ src/templete.js
@@ -1,25 +1,85 @@
 // dynamically create my html
-function profileGenerator() {
-  
-} 
-// function in a function is an object
-// function for engineer, intern and manager which returns html
-// html.push array
+function returnHtmlStringOfEmployees(Employee) {
+  return `
+  <!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <div class="container">        
+  ${returnHtmlStringOfManager({})}
+  </div>
+</body>
+</html>
+  `
+  }
+
+
+function returnHtmlStringOfManager(Manager) {
 return `
 <div class="card m-2 col-lg-3 p-0>
   <div class="card-body p-0">
    <div class="card-header bg-primary text-white">
-     <h3 class="card-title" >${this.Manager.name}</h3> 
-     <h5 class="card-subtitle mb-2">${this.Manager.email}</h5>
+     <h3 class="card-title" >${Manager.name}</h3> 
+     <h5 class="card-subtitle mb-2">${Manager.email}</h5>
     </div>
       <ul class="list-group list-group-flush">
-      <li class="list-group-item">${this.Manager.id}</li>      
+      <li class="list-group-item">${Manager.id}</li> 
+      <li class="list-group-item">${Manager.officenum}</li>     
  </div>
 </div>
 `  
+} 
 
 
-// module.export=(team)
+function returnHtmlStringOfIntern(Intern) {
+  return `
+  <div class="card m-2 col-lg-3 p-0>
+    <div class="card-body p-0">
+     <div class="card-header bg-primary text-white">
+       <h3 class="card-title" >${Intern.name}</h3> 
+       <h5 class="card-subtitle mb-2">${intern.id}</h5>
+      </div>
+        <ul class="list-group list-group-flush">
+        <li class="list-group-item">${Intern.email}</li> 
+        <li class="list-group-item">${Intern.school}</li>    
+   </div>
+  </div>
+  `
+  }
+  function returnHtmlStringOfEngineer(Engineer) {
+    return `
+    <div class="card m-2 col-lg-3 p-0>
+      <div class="card-body p-0">
+       <div class="card-header bg-primary text-white">
+         <h3 class="card-title" >${Engineer.name}</h3> 
+         <h5 class="card-subtitle mb-2">${Engineer.id}</h5>
+        </div>
+          <ul class="list-group list-group-flush">
+          <li class="list-group-item">${Engineer.email}</li> 
+          <li class="list-group-item">${Engineer.github}</li>    
+     </div>
+    </div>
+    `
+    }
 
+function linkForCss(){
+`
+<link rel="stylesheet" href="./style.css">
+`
+}
+function linkForBootstrap(){
+  `
+  
+  `
+}
+
+
+
+module.exports = returnHtmlStringOfEmployees
 // link style.css in the html
 // link bootstrap in the html
